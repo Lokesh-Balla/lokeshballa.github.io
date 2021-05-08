615eea278a641696df5537827d6947b30fd881a1
Lokesh-Balla <lokesh.balla123@gmail.com>
2021-05-08 15:18:20 +0530

    remebering dark mode choice using localStorage and adjusted the sizes to be viewable on mobile devices

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
+read_local_storage = () => {
+  if (localStorage.getItem("dark-mode") == "true") {
+    document.getElementById("dark-mode-switch").checked = true;
+  }
+};
+
 dark_mode_toggle = () => {
   if (document.getElementById("dark-mode-switch").checked == true) {
     document.body.classList.remove("light-mode");
     document.body.classList.add("dark-mode");
+    localStorage.setItem("dark-mode", "true");
   } else {
     document.body.classList.add("light-mode");
     document.body.classList.remove("dark-mode");
+    localStorage.setItem("dark-mode", "false");
   }
 };
 
 document.body.onload = function () {
+  read_local_storage();
   dark_mode_toggle();
 };
